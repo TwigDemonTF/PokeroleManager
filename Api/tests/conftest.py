06204ad586f351b4539fd60e8f46c8114d046bbe
TwigDemonTF <trollplay70@gmail.com>
2025-11-20 19:11:29 +0100

    trying to add python to root path

--- Api/tests/conftest.py
+++ Api/tests/conftest.py
@@ -3,6 +3,12 @@
 import pytest
 from Api import app, database
 
+import sys
+import os
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.insert(0, root_dir)
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
