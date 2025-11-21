845d98b17ece1251412d7e68a6bda2792cd06ab3
TwigDemonTF <trollplay70@gmail.com>
2025-11-21 17:31:23 +0100

    Update test_pokemon.py

--- Api/tests/test_pokemon.py
+++ Api/tests/test_pokemon.py
@@ -1,11 +1,6 @@
 # Test pokemon
  
 def test_pokemon_creation(client):
-    res = client.post("/register", json={
-        "username": twigdemon,
-        "password": twigdemon
-    })
-    
     response = client.post("/data", json={
         "GameId": "p15zow8i3k",
         "Name": "Airalin",
