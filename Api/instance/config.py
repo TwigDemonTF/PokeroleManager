db7e3f9862ce0e6af8655c0f3f9a1f320be2e92c
TwigDemonTF <trollplay70@gmail.com>
2025-12-14 23:40:48 +0000

    added preset config.py

---
+++ Api/instance/config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
+
+class ProductionConfig(Config):
+    DEBUG = False
